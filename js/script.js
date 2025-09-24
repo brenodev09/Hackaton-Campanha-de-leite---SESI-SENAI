cf5fc4515217b4a0476b0890eb16f982cf149f59
brenodev09 <brenolufy10@gmail.com>
2025-09-24 09:56:07 -0300

    inserção de pagina ranking

--- js/script.js
+++ js/script.js
@@ -2,8 +2,8 @@
 // script dos accordions - seçao de duvidas
 const accordions = document.querySelectorAll('.accordion')
 
-accordions.forEach (accordion => {
-    accordion.addEventListener('click', () =>{
+accordions.forEach(accordion => {
+    accordion.addEventListener('click', () => {
         const resposta = accordion.querySelector('.resposta-accordion')
         resposta.classList.toggle('ativo')
     })
@@ -11,3 +11,30 @@ accordions.forEach (accordion => {
 
 
 // dados das fileiras
+// script da seção de feedbacks - carrossel de depoimentos
+var swiper = new Swiper(".swiper", {
+    loop: true,
+    grabCursor: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true
+    },
+    breakpoints: {
+        640: {
+            slidesPerView: 1,
+            spaceBetween: 18
+        },
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 18
+        },
+        1188: {
+            slidesPerView: 3,
+            spaceBetween: 24
+        }
+    }
+});
